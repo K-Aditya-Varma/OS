d9b073f269c4e8675331d0cb2bae72b4eaf6b1fc
K-Aditya-Varma <155312361+K-Aditya-Varma@users.noreply.github.com>
2024-06-11 21:59:06 +0530

    Update Proportional Scheduling.c

--- Proportional Scheduling.c
+++ Proportional Scheduling.c
@@ -1,34 +1,51 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <time.h>
 typedef struct
 {
-  char name[5];
-  int T;
-} Task;
+    char name[5];
+    int tckts;
+} prc;
 
-int main()
+void main()
 {
-  int n;
-  float total_T = 0.0;
+    int n, ttl_tckts = 0;
+    float ttl_T = 0.0;
+    printf("Enter the no of Processes: ");
+    scanf("%d", &n);
+    prc p[n];
+    srand(time(NULL));
+    for (int i = 0; i < n; i++)
+    {
+        printf("\nProcess %d:\n", i + 1);
+        sprintf(p[i].name, "P%d", i + 1);
+        printf("Tickets: ");
+        scanf("%d", &p[i].tckts);
+        ttl_tckts += p[i].tckts;
+        ttl_T += p[i].tckts;
+    }
+    printf("Enter the time period for scheduling: ");
+    int m;
+    scanf("%d", &m);
+    for (int i=0;i<m;i++)
+    {
+        int win_tckt = rand() % ttl_tckts + 1;
+        int acc_tckts = 0;
+        int winner_index;
+        for (int j=0;j<n;j++)
+        {
+            acc_tckts += p[j].tckts;
+            if (win_tckt <= acc_tckts)
+            {
+                winner_index = j;
+                break;
+            }
+        }
+        printf("Tickets picked: %d, Winner: %s\n", win_tckt, p[winner_index].name);
+    }
+    for (int i = 0; i < n; i++)
+    {
+        printf("\nThe Process: %s gets %0.2f%% of Processor Time.\n", p[i].name, ((p[i].tckts / ttl_T) * 100));
+    }
 
-  printf("Enter the number of Processes: ");
-  scanf("%d", &n);
-
-  Task p[n];
-
-  for (int i = 0; i < n; i++)
-{
-    printf("\nProcess %d:\n", i + 1);
-    sprintf(p[i].name, "p%d", i + 1);
-    printf("Ticket: ");
-    scanf("%d", &p[i].T);
-    total_T += p[i].T;
-  }
-
-  for (int i = 0; i < n; i++)
-{
-    printf("\nThe Process: %s gets %0.2f%% of Processor Time.\n", p[i].name, ((p[i].T / total_T) * 100));
-  }
-
-  return 0;
 }
